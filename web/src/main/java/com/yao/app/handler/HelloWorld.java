1dd47664e5a01a6823683cf0b5975594dff53ef8
yaolei <yaolei313@gmail.com>
2015-12-12 23:03:14 +0800

    first

---
+++ web/src/main/java/com/yao/app/handler/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.yao.app.handler;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by yaolei on 15/11/13.
+ */
+public class HelloWorld extends AbstractHandler {
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello World 12123</h1>");
+        baseRequest.setHandled(true);
+    }
+}
